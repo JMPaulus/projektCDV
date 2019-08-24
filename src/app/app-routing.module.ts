8310b9ad771320d140222f765716143db8955d01
JMPaulus <codeboy@codeboy.pl>
2019-08-24 16:05:57 +0200

    Some mistakes corrected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CharactersComponent } from './characters/characters.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { PostersComponent } from './posters/posters.component';
 
+const Routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
 
-const routes: Routes = [];
+  },
+  {
+    path: 'characters',
+    component: CharactersComponent,
+  },
+  {
+    path: 'planets',
+    component: PlanetsComponent,
+  },
+  {
+    path: 'navigation',
+    component: NavigationComponent,
+  },
+  {
+    path: 'posters',
+    component: PostersComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  declarations: [
+    AppComponent
+    AppComponent,
+    CharactersComponent,
+    PlanetsComponent,
+    NavigationComponent,
+    HomeComponent,
+    PostersComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(Routes),
+    BrowserModule,
+    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
